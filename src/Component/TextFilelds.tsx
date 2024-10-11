b2b4a840b28fe0d366ac25bf8dcf2cc1c2443759
pranjal0927 <pranjal0927@gmail.com>
2024-10-11 13:12:50 +0530

    test

--- src/Component/TextFilelds.tsx
+++ src/Component/TextFilelds.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function TextFilelds() {
-    return (
-        <div>TextFilelds</div>
-    )
+  return (
+    <div>
+      <h1>
+        hello <textarea name="" id=""></textarea>
+      </h1>
+    </div>
+  );
 }
 
-export default TextFilelds
+export default TextFilelds;
